0f2337d2a8d8cc287dafb94adc83bcae546d6119
Tiago Aguiar <tiago.aguiar@moonjava.com.br>
2023-02-07 16:22:15 -0300

    Removido parte publica

---
+++ Sources/MeuLog.xcframework/ios-arm64_x86_64-simulator/MeuLog.framework/Headers/MeuLog.h
@@ -0,0 +1,18 @@
+//
+//  MeuLog.h
+//  MeuLog
+//
+//  Created by Tiago Aguiar on 07/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MeuLog.
+FOUNDATION_EXPORT double MeuLogVersionNumber;
+
+//! Project version string for MeuLog.
+FOUNDATION_EXPORT const unsigned char MeuLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MeuLog/PublicHeader.h>
+
+
