cdc6f652b5f82cf8723ce613a43fb752e32d24e0
Tiago Aguiar <tiago.aguiar@moonjava.com.br>
2023-02-07 16:06:01 -0300

    first commit

---
+++ Sources/MeuLog/Looger.swift
@@ -0,0 +1,14 @@
+//
+//  Looger.swift
+//  MeuLog
+//
+//  Created by Tiago Aguiar on 07/02/23.
+//
+
+import Foundation
+
+public class Logger {
+    static public func log(message: String) {
+        print("[LOGGER SPM] !!! : \(message)")
+    }
+}
